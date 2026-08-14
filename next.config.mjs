c0850fae05d8c0b0ff7b16945a2e13e82f5211fa
MAONEZZE <ruansanchezskt@gmail.com>
2026-08-14 15:44:27 -0300

    feat: add new remote pattern for sed-sup.akeel.com.br in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
         hostname: "**.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "sed-sup.akeel.com.br",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
     qualities: [75, 95],
   },
